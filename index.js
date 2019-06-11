4c6b97f3958f089dd8a5a019d296ebd4f5ebd8dd
Kurniadi Ahmad Wijaya <kurniadi_wijaya_25rpl@student.smktelkom-mlg.sch.id>
2019-06-11 13:44:48 +0700

    ADD | MongoDB , BodyParser Library. Initialize Body Parser And Connect Mongodb

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parsers')
 const router = require('./route')
+const mongoose = require('mongoose')
 
+// Connect MongoDB
+mongoose.connect('mongodb://localhost:27017/mobile-legend-heroes')
+mongoose.Promise = global.Promise
+
+// Root Route
 app.get('/api', function(req, res){
      res.end('403 Forbidden')
 })
 
+// Initial Body Parser
+app.use(bodyParser.json())
+
+// Initial Route
 app.use('/api', router)
 
 app.listen(process.env.port || 2019, function(){
