f9e3d9f45181e63863af47d858f2f8df8d702d93
Kurniadi Ahmad Wijaya <kurniadi_wijaya_25rpl@student.smktelkom-mlg.sch.id>
2019-06-11 12:41:49 +0700

    ADD | Base Endpoint API

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express')
 const app = express()
 
+app.get('/api', function(req, res){
+     res.end('403 Forbidden')
+})
+
+app.get('/api/siswa', function(req, res){
+    res.send({
+        nama: "Kurniadi Ahmad Wijaya",
+        umur:27
+    })
+})
+
 app.listen(process.env.port || 2019, function(){
     console.log('Server Berhasil Dibuat')
 });
